10dd289e75bc4fb6267a527f075de46f3b185d03
rashmi suryavanshi <rashmisuryavanshi46@email.com>
2026-05-12 15:15:18 +0530

    update production backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" 
+  : "https://rt-chat-app-sxx0.onrender.com/api",
   withCredentials: true,
 });
