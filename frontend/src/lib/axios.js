f6acf2c188c9339eaa9df139c4e95b7bb42bfb3b
rashmi suryavanshi <rashmisuryavanshi46@email.com>
2026-05-13 00:25:11 +0530

    env setup

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" 
-  : "https://rt-chat-app-sxx0.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
