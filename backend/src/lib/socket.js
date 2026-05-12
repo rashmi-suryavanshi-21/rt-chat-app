61559dadcd2682832e31a61208228ae3a633365d
rashmi suryavanshi <rashmisuryavanshi46@email.com>
2026-05-13 02:00:56 +0530

    socket fix

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://rt-chat-app-lovat.vercel.app"
+    ],
     credentials: true,
   },
 });
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ res.cookie("jwt", token, {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "none",
-  secure: process.env.NODE_ENV !== "development",
+  secure: true,
 });
 
   return token;
